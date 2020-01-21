231e64cd6a326ed2f23cf3db14da859f84ef8423
oluwaseyi.ayodele <oluwaseyi.ayodele@arca.network>
2020-01-21 15:53:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/simpledagger/remote/UserService.java
@@ -0,0 +1,12 @@
+package com.example.simpledagger.remote;
+
+import com.example.simpledagger.model.UserModel;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+
+public interface UserService {
+
+    @GET("/users/2")
+    Single<UserModel> getUserModel();
+}
